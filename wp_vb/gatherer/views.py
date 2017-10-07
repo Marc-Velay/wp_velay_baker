aa7deed1a48d8f8466ed95fb3b6c1eb87d880710
Alistair-Baker <bakeralistair95@gmail.com>
2017-10-07 16:15:15 +0200

    Added loading of CSV into Item table

--- wp_vb/gatherer/views.py
+++ wp_vb/gatherer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import csv
 from django.conf import settings
 from datetime import datetime
+from gatherer.models import Item
 
 # Create your views here.
 
@@ -17,16 +18,23 @@ def index(request):
 def createDateTime(date,time):
     DateAndTime = date + " " + time
     dt = datetime.strptime(DateAndTime, '%Y.%m.%d %H:%M')
-    print (dt)
     return dt
 
 #CSV loader function
 def loadFromCSV():
-    print("Hello")
     with open('media/gatherer/DAT_MT_EURUSD_M1_201709.csv', 'rt') as f:
         reader = csv.reader(f, delimiter = ",")
         for row in reader:
             DateAndTime = createDateTime(row[0],row[1])
+            _, created = Item.objects.get_or_create(
+                name = "Forex",
+                source = "HistData.com",
+                timestamp = DateAndTime,
+                opening = row[2],
+                high = row[3],
+                low = row[4],
+                closing = row[5],
+                )
 
 def test(request):
     '''
