195fbef34355fdf289d8ebf63b810b180ef3d562
Alistair-Baker <bakeralistair95@gmail.com>
2017-10-07 13:17:39 +0200

    Concatenated date and time and converted it to right format

--- wp_vb/gatherer/views.py
+++ wp_vb/gatherer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import csv
 from django.conf import settings
+from datetime import datetime
 
 # Create your views here.
 
@@ -12,13 +13,20 @@ def index(request):
     loadFromCSV()
     return render(request, 'gatherer/index.html', context)
 
+#Function to format date and time correctly
+def createDateTime(date,time):
+    DateAndTime = date + " " + time
+    dt = datetime.strptime(DateAndTime, '%Y.%m.%d %H:%M')
+    print (dt)
+    return dt
+
+#CSV loader function
 def loadFromCSV():
     print("Hello")
     with open('media/gatherer/DAT_MT_EURUSD_M1_201709.csv', 'rt') as f:
         reader = csv.reader(f, delimiter = ",")
-        for line in reader:
-            print (line)
-
+        for row in reader:
+            DateAndTime = createDateTime(row[0],row[1])
 
 def test(request):
     '''
