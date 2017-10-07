4fbe3a7413f8005e7937747ebd467a1ba884730d
Alistair-Baker <bakeralistair95@gmail.com>
2017-10-07 12:46:16 +0200

    Added Item model to load CSV into it

--- wp_vb/gatherer/models.py
+++ wp_vb/gatherer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Stock and item model
+class Item(models.Model):
+    name = models.CharField(max_length = 30)
+    source = models.CharField(max_length = 30)
+    timestamp = models.DateTimeField()
+    opening = models.DecimalField(decimal_places=10)
+    high = models.DecimalField(decimal_places=10)
+    low = models.DecimalField(decimal_places=10)
+    closing =  models.DecimalField(decimal_places=10)
