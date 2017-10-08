5b7570de2c9e0a6d3be6cc7e3753791cfceda983
Marc-Velay <marc.velay@gmail.com>
2017-10-09 01:01:13 +0200

    restructured project, separating client and backend. Demo front atm

--- wp_vb/server/config/settings.py
+++ wp_vb/server/config/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'wp_vb.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wp_vb.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
