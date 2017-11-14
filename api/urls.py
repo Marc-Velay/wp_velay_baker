48424a77cb03ae925abce1f15fd532e36d6c0023
Alistair-Baker <bakeralistair95@gmail.com>
2017-11-14 11:24:01 +0100

    Added url paths to query item DB Added new view classes to call Items

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,17 @@
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from api.views import CreateView, DetailsView
+from api.views import CreateView, DetailsView, item_year, item_day, item_month
 
 urlpatterns = {
     url(r'^bucketlists/$', CreateView.as_view(), name="create"),
     url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
     url(r'^items/$', CreateView.as_view(), name="create"),
+    url(r'^(?P<item>\w+)/',include([
+        url(r'^(?P<year>[0-9]{4})/$', item_year.as_view(), name="year"),
+        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', item_day.as_view(), name="day"),
+        url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', item_month.as_view(), name="month"),
+    ])),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from api.serializers import ItemSerializer,BucketlistSerializer
 from api.models import Item,Bucketlist
+import datetime
 
 # Create your views here.
 
@@ -15,7 +16,7 @@ class CreateView(generics.ListCreateAPIView):
     serializer_class = ItemSerializer
 
     def perform_create(self, serializer):
-        """Save the post data when creating a new bucketlist."""
+        """Save the post data when creating a new Item."""
         serializer.save()
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
@@ -23,3 +24,45 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     # Handles REST ( GET, PUT, DELETE )
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+class item_year(generics.ListCreateAPIView):
+
+    #Fetch correct item serializer
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        """
+        This view returns all entries for the given year through the URL
+        """
+        item = self.kwargs['item']
+        year = self.kwargs['year']
+        return Item.objects.filter(name = item, timestamp__year = year)[:100]
+
+class item_month(generics.ListCreateAPIView):
+
+    #Fetch correct item serializer
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        """
+        This view returns all entries for the given month for the given item through the URL
+        """
+        item = self.kwargs['item']
+        year = self.kwargs['year']
+        month = self.kwargs['month']
+        return Item.objects.filter(name = item, timestamp__year = year, timestamp__month = month)[:100]
+
+class item_day(generics.ListCreateAPIView):
+
+    #Fetch correct item serializer
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        """
+        This view returns all entries for the given day through the URL
+        """
+        item = self.kwargs['item']
+        year = self.kwargs['year']
+        month = self.kwargs['month']
+        day = self.kwargs['day']
+        return Item.objects.filter(name = item, timestamp__year = year, timestamp__month = month, timestamp__day = day)[:100]
